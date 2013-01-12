6ef8530b1ed7eec719543dad32aad15fd59cb6e3
Dutch Boyd <dutch@dutchboyd.com>
2013-01-12 00:38:59 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Namefountain::Application.config.secret_token = '20ce3a41f7ea7e283cc38478d31b2752cb04fe82d9122675d5ed8ea8589d9f8ff48b332278d2150caa3c502870d0877d2ff4de8857cbe0613ca156bb59e15765'
